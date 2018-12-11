fa04ea6f3fe083316ac348041ff8a43162a92e19
Rob Flynn <rob@thingerly.com>
2018-12-10 21:38:59 -0500

    dont worry about key

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/home/rob/services/webster-api"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-append :linked_files, "config/master.key"
+# append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
