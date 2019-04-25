873c737241920306aaeb69d80fbbe4da5fac3c3c
Rob Flynn <rob@thingerly.com>
2019-04-25 16:38:00 -0400

    Tweak enum formatting

--- app/models/page.rb
+++ app/models/page.rb
@@ -22,7 +22,16 @@
 #
 
 class Page < ApplicationRecord
-  enum status: [:uncrawled, :crawling, :crawled, :processing, :processed, :crawl_error, :unknown_type_error, :skipped]
+  enum status: [
+    :uncrawled,
+    :crawling,
+    :crawled,
+    :processing,
+    :processed,
+    :crawl_error,
+    :unknown_type_error,
+    :skipped
+  ]
 
   scope :visited,  -> { where(status: [:skipped, :crawl_error, :crawled]) }
   scope :not_visited, -> { where(status: [:uncrawled, :crawling]) }
